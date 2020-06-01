d555d222fc6205aba36bdd744b298b92ed830b04
Rafael <rvillegasm@eafit.edu.co>
2020-06-01 16:14:16 -0500

    cambios

--- src/crypto.rs
+++ src/crypto.rs
@@ -3,18 +3,18 @@ use sodiumoxide::crypto;
 /// Struct used to manage all the encryption logic
 #[derive(Clone)]
 pub struct Decrypter {
-    pub public_key: Vec<u8>,
-    private_key: Vec<u8>,
+    pub box_public_key: crypto::box_::PublicKey,
+    box_private_key: crypto::box_::SecretKey,
 }
 
 impl Decrypter {
     /// Creates a new `Encrypter` instance with private and public keys
     pub fn new() -> Decrypter {
-        let (public_key, private_key) = crypto::box_::gen_keypair();
+        let (box_public_key, box_private_key) = crypto::box_::gen_keypair();
 
         Decrypter {
-            public_key: public_key.0.to_vec(),
-            private_key: private_key.0.to_vec(),
+            box_public_key,
+            box_private_key,
         }
     }
 
@@ -32,9 +32,8 @@ impl Decrypter {
     pub fn open(&self, msg: &[u8], nonce: &[u8], other_pk: &[u8]) -> Vec<u8> {
         let nonce = crypto::box_::Nonce::from_slice(nonce).unwrap();
         let other_pk = crypto::box_::PublicKey::from_slice(other_pk).unwrap();
-        let my_secret_key = crypto::box_::SecretKey::from_slice(&self.private_key).unwrap();
 
-        crypto::box_::open(msg, &nonce, &other_pk, &my_secret_key)
+        crypto::box_::open(msg, &nonce, &other_pk, &self.box_private_key)
             .expect("Could not open the message")
     }
 }
